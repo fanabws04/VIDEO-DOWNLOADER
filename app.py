f65476314fe2e8b1ebe34d451c2ab17a0c76da02
fanabws04 <111634720+fanabws04@users.noreply.github.com>
2022-08-20 17:39:14 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request, redirect
+import youtube_dl
+
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+	return render_template('index.html')
+  
+@app.route('/download', methods=["POST", "GET"])
+def download():
+	try:
+		url = request.form["url"]
+		with youtube_dl.YoutubeDL() as ydl:
+
+			url = ydl.extract_info(url, download=False)
+
+			try:
+				download_link = url["entries"][-1]["formats"][-1]["url"]
+			except:
+				download_link = url["formats"][-1]["url"]
+
+			if ".mp4" in download_link:
+				return redirect(download_link)
+			else:
+				return redirect(download_link+"&dl=1")
+	except:
+		return render_template('index.html')
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0',port=8000, debug=False)
